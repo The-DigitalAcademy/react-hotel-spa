96ace4ad1f9693631179d14aebed823d776cc3d4
IreneMonchwe <bellamonchwe@gmail.com>
2023-06-01 14:11:54 +0200

    add new logo

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,10 +4,17 @@ import '../index.css';
 
 const Carousel = () => {
   return (
-    <div>
+    <div className='containerVideo'>
+    <img
+    className='videoTitle2' 
+    src="https://www.saxon.co.za/app/uploads/2022/10/saxon-logo.svg"
+    alt="new"
+    />
       <video className="videoTag" autoPlay loop muted>
         <source src={sample} type="video/mp4" />
       </video>
+     
+      <span className='videoTitle'>YOUR SOUTH AFRICAN EXPERIENCE BEGINS HERE</span>
     </div>
   )
 }
